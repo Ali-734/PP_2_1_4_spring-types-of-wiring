507e06bc178298ef6dbe28f4bcdfba2c948d75fc
Ali-734 <ali2004mah@mail.ru>
2024-07-11 21:46:31 +0300

    delaeshh

---
+++ src/main/java/koschei/models/Duck5.java
@@ -0,0 +1,19 @@
+package koschei.models;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Duck5 {
+    @Autowired
+    private Egg6 egg1;
+
+    public Duck5(Egg6 egg1) {
+        this.egg1 = egg1;
+    }
+
+    @Override
+    public String toString() {
+        return ", в утке яйцо " + egg1.toString();
+    }
+}
